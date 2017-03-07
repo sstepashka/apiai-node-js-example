356d32b0eef6632219c7d07ae89d6000e75d321f
Dmitriy Kuragin <kuragin@speaktoit.com>
2017-03-07 13:27:54 +0700

    Initial commit

---
+++ text_request.ts
@@ -0,0 +1,19 @@
+/// <reference types="apiai" />
+
+"use strict";
+
+import * as apiai from "apiai";
+
+const app = apiai("YOUR_ACCESS_TOKEN");
+
+let request = app.textRequest("Hello", {sessionId: "UNIQUE_SESSION_ID"});
+
+request.on("response", function (response) {
+    console.log("response: " + JSON.stringify(response, null, "  "));
+});
+
+request.on("error", function(error) {
+    console.log("error: " + error);
+});
+
+request.end();
